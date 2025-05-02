58f4922b6d3e422cc62fdde6fa41838ce6578818
Edgar Rodriguez Alfonso <70775577+CaptainBitwise@users.noreply.github.com>
2025-05-02 00:14:41 -0600

    Actualizar routes.tsx

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter ([
         element: <GalleryPage />
     },
     {
-        path: '/auth',
+        path: '/authentication',
         element: <AuthPage/>
     }
 ])
